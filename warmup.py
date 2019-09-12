18474056eb0342d1f7bc8a30da1e7acf69be32b5
Pierce Edmiston <pierce@8thlight.com>
2019-09-12 17:41:21 -0500

    Refactor

--- warmup.py
+++ warmup.py
@@ -1,8 +1,11 @@
+def find_first_match(items, name, default=None):
+    return next((item for item in items if item["name"] == name), default)
+
 items = [
     {"id": 33, "name": "bob"},
     {"id": 44, "name": "uncle"},
     {"id": 55, "name": "joe"},
     {"id": 66, "name": "uncle"},
 ]
-print(next((item for item in items if item["name"] == "uncle"), None))
-print(next((item for item in items if item["name"] == "jim"), None))
+print(find_first_match(items, "uncle"))
+print(find_first_match(items, "jim"))
