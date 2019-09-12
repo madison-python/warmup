6520a6a5948bbade735ac696ac3e76b566a83daf
Pierce Edmiston <pierce@8thlight.com>
2019-09-12 17:35:55 -0500

    Bert Barabas

--- warmup.py
+++ warmup.py
@@ -1,16 +1,8 @@
-import pathlib
-import shutil
-import zipfile
-
-
-def extract_txt(zip_filename, dst_dir):
-    """Extract all txt files from a zip."""
-    with zipfile.ZipFile(zip_filename) as zip_src:
-        names = [
-            name for name in zip_src.namelist()
-            if pathlib.Path(name).suffix == ".txt"
-        ]
-        for member in names:
-            dst_path = pathlib.Path(dst_dir) / pathlib.Path(member).name
-            with zip_src.open(member) as src, open(dst_path, "wb") as dst:
-                shutil.copyfileobj(src, dst)
+items = [
+    {"id": 33, "name": "bob"},
+    {"id": 44, "name": "uncle"},
+    {"id": 55, "name": "joe"},
+    {"id": 66, "name": "uncle"},
+]
+print(next((item for item in items if item["name"] == "uncle"), None))
+print(next((item for item in items if item["name"] == "jim"), None))
